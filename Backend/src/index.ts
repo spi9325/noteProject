d3a09d5b108d2f6bbd7b0b798e8155a70fd0a2f4
spi9325 <sanketinamdar72@gmail.com>
2025-03-14 18:52:50 +0530

    add fe url to be

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -6,13 +6,11 @@ import * as dotenv from 'dotenv';
 import { notesRoute } from "./routes/notesRoute";
 dotenv.config();
 
-const frontendUrl = process.env.FRONTEND_URL;
-
 const app = express();
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin:"http://localhost:3000",
+  origin:process.env.FRONTEND_URL,
   credentials: true
 }))
 
