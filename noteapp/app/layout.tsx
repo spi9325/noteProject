407b64e4737bf31f9b084b85b9604f86804dd529
spi9325 <sanketinamdar72@gmail.com>
2025-03-15 12:38:09 +0530

    add myprovider to root layout

--- noteapp/app/layout.tsx
+++ noteapp/app/layout.tsx
@@ -19,11 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
         <body >
-        
-         
+         <MyProvider>
             {children}
-          
-        
+         </MyProvider>
         </body>
     </html>
   );
