822e764b361369dfdbeb123bcb9efc2917c9dc4f
spi9325 <sanketinamdar72@gmail.com>
2025-03-16 14:40:33 +0530

    fix mobile menu bug

--- noteapp/components/Navbar.tsx
+++ noteapp/components/Navbar.tsx
@@ -5,22 +5,39 @@ import { HiBars3 } from "react-icons/hi2";
 import {ScrollTrigger} from "gsap/ScrollTrigger"
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap"
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import axios from "axios";
 
 gsap.registerPlugin(ScrollTrigger)
 
 export function Navbar(){
     const [authorized,setAuthorized] =useState(false);
-    const t1= gsap.timeline()
+    const t1 = useRef<gsap.core.Timeline | null>(null);
     function handelClick(){
-        t1.play()
+        t1.current?.play();
     }
     function hidediv(){
-        t1.reverse()
+        t1.current?.reverse()
     }
-
     
+    useEffect(() => {
+        if (!t1.current) {
+            t1.current = gsap.timeline({ paused: true });
+
+            t1.current.to("#mobile-menu", {
+                right: 20,
+                duration: 0.5,
+            });
+
+            t1.current.from("#mobile-menu-text", {
+                x: "10px",
+                duration: 0.1,
+                opacity: 0,
+                stagger: 0.5,
+            });
+        }
+    }, []);
+
        useGSAP(()=>{
             gsap.from(".logo",{
                 y:-100,
@@ -38,26 +55,14 @@ export function Navbar(){
                 opacity:0,
             })
  
-            t1.to("#mobile-menu",{
-                right:20,
-                duration:0.5,
-            })
-            t1.from("#mobile-menu-text",{
-                x:"10px",
-                duration:0.1,
-                opacity:0,
-                stagger:0.5
-            })
+           
             
-            t1.pause(); 
-       }) 
+       },[]) 
        useEffect(()=>{
         async function validate(){
             const res = await axios.get(`${process.env.NEXT_PUBLIC_Backend_URL}/user/authorized`,{withCredentials:true});
             console.log(res.data + "data true")
-            if(res.data === true){
-                setAuthorized(true);
-            }
+           setAuthorized(res.data === true)
         }
         validate();
     },[])
@@ -82,17 +87,17 @@ export function Navbar(){
                     }
                     
 
-                    <div onClick={handelClick} className="w-[50px] text-2xl sm:hidden flex items-center justify-center sign">
+                    <div onClick={()=>handelClick()} className="w-[50px] text-2xl sm:hidden flex items-center justify-center sign">
                             <HiBars3 />
                     </div>
                 </div>
-
+                        
                          <div id="mobile-menu" className=" absolute top-12 right-[-90%] bg-black text-white flex flex-col p-3 w-[250px] justify-center items-center gap-4 rounded-lg ">
                                 <p onClick={()=>hidediv()} className="absolute top-0 right-0 px-2 rounded mr-5 mt-1 font-medium text-black bg-green-300 cursor-pointer">X</p>
-                                <Link id="mobile-menu-text" className="mt-3 rounded-lg outline-none tracking-widest transition-shadow duration-100 hover:shadow-white px-10 " href={"/"}>Home</Link>
-                                <Link id="mobile-menu-text" className="tracking-widest rounded-lg transition-shadow duration-100 hover:shadow-white px-10 barsmenus" href={"/services"}>Services</Link>
-                                <Link id="mobile-menu-text" className="tracking-widest rounded-lg transition-shadow duration-100 hover:shadow-white px-10 barsmenus" href={"/manuals"}>Manual</Link>
-                                <Link id="mobile-menu-text" className="tracking-widest rounded-lg transition-shadow duration-100 hover:shadow-white px-10 barsmenus" href={"/about"}>About</Link>
+                                <Link id="mobile-menu-text" className=" mt-3 rounded-lg outline-none tracking-widest transition-shadow duration-100 hover:shadow-white px-10 " href={"/"}>Home</Link>
+                                <Link id="mobile-menu-text" className=" tracking-widest rounded-lg transition-shadow duration-100 hover:shadow-white px-10 barsmenus" href={"/services"}>Services</Link>
+                                <Link id="mobile-menu-text" className=" tracking-widest rounded-lg transition-shadow duration-100 hover:shadow-white px-10 barsmenus" href={"/manuals"}>Manual</Link>
+                                <Link id="mobile-menu-text" className=" tracking-widest rounded-lg transition-shadow duration-100 hover:shadow-white px-10 barsmenus" href={"/about"}>About</Link>
                             </div>
 
                     
